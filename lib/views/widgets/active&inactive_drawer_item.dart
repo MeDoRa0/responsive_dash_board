c2f9b0069b4b066d76efda416638f083b9a00c1e
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2024-01-08 23:52:10 +0200

    active and inactive drawer item done

---
+++ lib/views/widgets/active&inactive_drawer_item.dart
@@ -0,0 +1,52 @@
+//in case the drawer item is not solected
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
+import 'package:responsive_dash_board/utlis/app_styles.dart';
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.icon),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleRegular16,
+      ),
+    );
+  }
+}
+
+// in case the drawer item is selected
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.icon),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleBold16,
+      ),
+      trailing: Container(
+        width: 3.27,
+        decoration: const BoxDecoration(
+          color: Color(0xFF4EB7F2),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/widgets/drawer_item.dart
+++ lib/views/widgets/drawer_item.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dash_board/models/drawer_item_model.dart';
-import 'package:responsive_dash_board/utlis/app_styles.dart';
+import 'package:responsive_dash_board/views/widgets/active&inactive_drawer_item.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key, required this.drawerItemModel, required this.isActive});
+  const DrawerItem(
+      {super.key, required this.drawerItemModel, required this.isActive});
   //we bring the drawer item model because we will use it here
   final DrawerItemModel drawerItemModel;
   final bool isActive;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(drawerItemModel.icon),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleRegular16,
-      ),
-    );
+    return isActive
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
   }
 }
